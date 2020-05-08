53ce5e374e613afaa72642cd3309ecc5da269198
Jhojan <pipeflx85@gmail.com>
2020-05-08 14:24:36 -0500

    Add Password API

--- libros/serializers.py
+++ libros/serializers.py
@@ -1,11 +1,23 @@
 from django.contrib.auth.models import User
 from .models import Libro
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password', 'placeholder': 'Contraseña'}
+    )
+
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'first_name', 'password']
+    
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data.get('password'))
+        return super(UserSerializer, self).create(validated_data)
 
 class BookSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
